fcf3b2ffa8f8d14183caf0c3f6fae4ecd7fe9711
Yoav Luft <yoav.luft@prototyp.se>
2024-01-31 14:17:55 +0100

    fix: WindowFunctions.elm was not exposed, also added some comments and some extra common windows

--- src/WindowFunctions.elm
+++ src/WindowFunctions.elm
@@ -1,11 +1,36 @@
-module SamplingWindows exposing (..)
-
-import List.Extra
-
+module WindowFunctions exposing
+    ( impulse
+    , square, centeredSquare
+    , blackman, hann, hamming
+    )
 
 {-| Different windows used for sampling when working with FFT and convolution algorithms.
 See: <https://en.wikipedia.org/wiki/Window_function>
+
+
+# Impulse
+
+The impulse is the simplest window, it takes only the first value
+
+@docs impulse
+
+
+# Square
+
+Square is just a list of ones (1). A centered square window is padded by zeros on both sides.
+
+@docs square, centeredSquare
+
+
+# Cosine-sum windows
+
+These windows use the sum of cosine functions to create the window. They are very common in filtering.
+
+@docs blackman, hann, hamming
+
 -}
+
+import List.Extra
 
 
 {-| The impulse window is a window in which the first element is 1, and all other elements are zero
@@ -56,8 +81,8 @@ centeredSquare size width =
 {-| returns a Blackman window of size n.
 See: <https://en.wikipedia.org/wiki/Window_function#Blackman_window>
 -}
-blackmanWindow : Int -> List Float
-blackmanWindow size =
+blackman : Int -> List Float
+blackman size =
     let
         a0 =
             0.42
@@ -74,3 +99,27 @@ blackmanWindow size =
                 - (a1 * cos (2 * pi * toFloat i / toFloat size))
                 + (a2 * cos (4 * pi * toFloat i / toFloat size))
         )
+
+
+cosineSum : Float -> Int -> List Float
+cosineSum a0 size =
+    List.Extra.initialize size
+        (\i ->
+            a0 - ((1 - a0) * cos (2 * pi * toFloat i / toFloat size))
+        )
+
+
+{-| hann window is a cosine sum window with a0 parameter set to 0.5.
+See: <https://en.wikipedia.org/wiki/Window_function#Hann_and_Hamming_windows>
+-}
+hann : Int -> List Float
+hann =
+    cosineSum 0.5
+
+
+{-| hann window is a cosine sum window with a0 parameter set to 0.54.
+See: <https://en.wikipedia.org/wiki/Window_function#Hann_and_Hamming_windows>
+-}
+hamming : Int -> List Float
+hamming =
+    cosineSum (25 / 46)
